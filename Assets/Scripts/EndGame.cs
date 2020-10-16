620ea66dbee5761736704e1f9c18ac4f9811f648
bit1creative <bit1creative@gmail.com>
2020-10-16 15:54:00 +0300

    first commit

---
+++ Assets/Scripts/EndGame.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGame : MonoBehaviour
+{
+    public Rigidbody rb;
+    public GameObject youWinText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Invoke("Freeze", 0.1f);
+    }
+
+    public void Freeze()
+    {
+        youWinText.SetActive (true);
+        Time.timeScale = 0f;
+    }
+
+
+}
+
---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGame : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float force;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Invoke("HitBall", 3f);
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void HitBall()
+    {
+        rb.AddForce(new Vector3(0f, 0f, force), ForceMode.Impulse);
+    }
+}
